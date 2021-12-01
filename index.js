95a41959965d594065790cdeaf05ce43b923bc4b
Itacir Gabral <itacirgabral@gmail.com>
2021-12-01 11:33:24 -0400

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const fs = require('fs')
+const { WAConnection, MessageType, Mimetype } = require('@adiwajshing/baileys')
+
+const someone = '554433221100@s.whatsapp.net'
+const image = fs.readFileSync(require('path').join(__dirname, 'gretchen.jpeg'))
+const messageType = MessageType.image
+const options = {
+  mimetype: Mimetype.image,
+  filename: 'gretchen.jpeg'
+}
+
+;(async () => {
+    const conn = new WAConnection()
+
+    await conn.connect()
+
+    setTimeout(() => {
+        console.log('bep')
+        conn
+          .sendMessage (someone, image, messageType, options)
+          .then(console.dir)
+          .catch(console.erro)
+    }, 1_000)
+})()
